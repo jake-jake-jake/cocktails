60fd8536ca8d0d5f747c5f1a8fce03e5c1f7f5aa
Jake Davis <jcdavis@awedge.net>
2016-06-02 15:08:18 -0400

    admin/model changes

--- cocktails/drinks/admin.py
+++ cocktails/drinks/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Drink, Ingredient, IngredientLine
 
 # Register your models here.
+
+
 admin.site.register(Drink)
 admin.site.register(IngredientLine)
 admin.site.register(Ingredient)
+
 
