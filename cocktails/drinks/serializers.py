56632b4fd0b3b0b5faf2ed05ddb2bbea485c728d
Jake Davis <jcdavis@awedge.net>
2016-06-02 16:11:03 -0400

    More serializers

--- cocktails/drinks/serializers.py
+++ cocktails/drinks/serializers.py
@@ -2,9 +2,17 @@ from rest_framework import serializers
 from drinks.models import Drink, Ingredient, IngredientLine
 
 
-class DrinkSerializer(serializers.Serializer):
+class DrinkSerializer(serializers.ModelSerializer):
     ings = serializers.StringRelatedField(many=True)
-
+    name = serializers.CharField()
+    instructions = serializers.CharField()
     class Meta:
         model = Drink
         fields = ('name', 'instructions', 'ings')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ('name', 'abv', 'type')
+
--- cocktails/drinks/views.py
+++ cocktails/drinks/views.py
@@ -1,8 +1,60 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from drinks.models import Drink, Ingredient
+from drinks.serializers import DrinkSerializer
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def index(request, question_id):
+def index(request):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
+
+def JSONResponse(HttpResponse):
+    ''' Render data as JSON''' 
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def drink_list(request):
+    """
+    List all drinks.
+    """
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = DrinkSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def ingredient_list(request):
+    """
+    List all ingredients.
+    """
+    if request.method == 'GET':
+        ingredients = Ingredient.objects.all()
+        serializer = IngredientSerializer(ingredients, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = IngredientSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
