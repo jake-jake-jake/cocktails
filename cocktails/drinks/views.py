93cd87c494c2a2a26d683d2b24f44954bb92ff2d
jake-jake-jake <jcdavis@awedge.net>
2016-06-02 19:11:42 -0400

    clean up routebased views, add IngredientList

--- cocktails/drinks/views.py
+++ cocktails/drinks/views.py
@@ -1,5 +1,6 @@
 from drinks.models import Drink, Ingredient
-from drinks.serializers import DrinkSerializer
+from drinks.serializers import DrinkSerializer, IngredientSerializer
+from drinks.permissions import IsOwnerOrReadOnly
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -9,25 +10,20 @@ from rest_framework import generics
 from rest_framework import permissions
 
 
-# path based views
-def index(request):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def JSONResponse(HttpResponse):
-    ''' Render data as JSON''' 
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
 # class based views
 class DrinkList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class IngredientList(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
