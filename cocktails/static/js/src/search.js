f68ac2280965997ae07b84a4215a39f9734479a5
Jake Davis <jcdavis@awedge.net>
2016-06-07 11:41:57 -0400

    Try removing state from search box

--- cocktails/static/js/src/search.js
+++ cocktails/static/js/src/search.js
@@ -1,23 +1,15 @@
 var React = require('react');
 
 var Search = React.createClass({
-  getInitialState: function() {
-    return {
-      input: "searchInput"
-    }
-    
-  },
-  
-  mySearch: function() {
-    let value = document.getElementById( this.state.input ).value;
-    this.props.search( value );
+  mySearch: function(e) {
+    this.props.search( e.target.value.trim() );
   },
   
   render: function() {
     return (
       <div className = "flexCol search">
-        <input  id = { this.state.input }
-                placeholder = "Enter ingredient to search"
+        <input  type="text"
+                value = "Enter ingredient to search"
                 onSubmit    = { this.mySearch } />
         <button value   = "Search"
                 onClick = { this.mySearch } />
