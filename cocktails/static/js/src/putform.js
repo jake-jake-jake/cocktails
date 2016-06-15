e36882514e04f443db9ce291d4ea882bc4af4c24
jake-jake-jake <jcdavis@awedge.net>
2016-06-15 11:36:58 -0400

    Fix comments.

--- cocktails/static/js/src/putform.js
+++ cocktails/static/js/src/putform.js
@@ -1,38 +1,59 @@
 var React = require('react');
 
+/* Super rad modular form submitter. It makes a put request to the url in the props. 
+** Form fields dynamically set via the array in the fields property. Observe default
+** props for more info.
+** Or modify this obj as you need.
+             {header: "A Put Form",
+              fields: [{placeholder: "Email placeholder", type: "text", name: "Email"},
+                       {placeholder: "Name placeholder", type: "text", name: "Name"},
+                       {placeholder: "Addr placeholder", type: "text", name: "Address"}],
+              url: "/unset/url",
+              className: "putForm",
+              buttonText: "Button"
+             };
+*/
+
 var PutForm = React.createClass({
   getDefaultProps: function() {
       return {header: "A Put Form",
-              fields: [{placeholder: "A placeholder", type: "text"},
-                       {placeholder: "Another placeholder", type: "text"},
-                       {placeholder: "Another placeholder", type: "text"}],
+              fields: [{placeholder: "Email placeholder", type: "text", name: "Email"},
+                       {placeholder: "Name placeholder", type: "text", name: "Name"},
+                       {placeholder: "Addr placeholder", type: "text", name: "Address"}],
               url: "/unset/url",
               className: "putForm",
               buttonText: "Button"
              };
   },
 
+  getInitialState: function() {
+    return {}  
+  },
+
+  updateField: function(elem, event) {
+    var name = elem.name;
+    let value = event.target.value;
+    this.setState({[elem.name]: value})
+  },
+
   putContent: function(e) {
-    // Query API for drinks using ingredient tied to primary key
+    // Make a PUT request from the content of the form, derived from this.state values,
+    // to the address at this.props.url
     e.preventDefault();
     console.log('You submitted the button.');
-    console.log('This was the passed eect: ');
-    console.log(e)
-    return null
     if(!e) {
       console.log('Set some error handler here');
       return null;
     }
     let xhr = new XMLHttpRequest();
-    let url = this.props.drinksURL + pk.toString() + '.json'
-    if(url==this.state.lastDrinkSearch){
-      return
-    }
-    xhr.open("put", url, true);
+    let url = this.props.url
+    xhr.open("PUT", url, true);
     xhr.onload = function() {
-      let ingredient = JSON.parse(e);
+      let ingredient = this.state;
+      console.log(ingredient)
       xhr.send();
-    }
+    }.bind(this);
+    xhr.send();
   },
 
   render: function() {
@@ -41,8 +62,9 @@ var PutForm = React.createClass({
       <div className={this.props.className}>
         <h3>{this.props.header}</h3>
         <form onSubmit={this.putContent}>
-          {formelements.map(function(elem) {
-          return <input {...elem} />
+          {formelements.map(function(elem, i) {
+            var updateField = this.updateField.bind(this, elem)
+            return <input key={i} onChange={updateField} {...elem} />
           }, this)}
           <input type="submit" value={this.props.buttonText} />
         </form>
